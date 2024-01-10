eb7e67feff511085e6109bc73b8c37d3dd4da027
Ted Moens <ted.moens@gmail.com>
2024-01-10 12:14:57 -0800

    fix: adjust category menu sizes to fit new category names

--- src/app/menus/vr-ranking-category-selector/vr-ranking-category-selector.component.ts
+++ src/app/menus/vr-ranking-category-selector/vr-ranking-category-selector.component.ts
@@ -5,8 +5,8 @@ import {
   JR_CATEGORY_HIERARCHY,
   ADULT_CATEGORY_HIERARCHY,
   SR_CATEGORY_HIERARCHY,
-} from "../../../assets/rankings-categories";
-import {RankingCategory} from "../../utils/ranking-category";
+} from '../../../assets/rankings-categories';
+import {RankingCategory} from '../../utils/ranking-category';
 
 @Component({
   selector: 'app-vr-ranking-category-selector',
@@ -20,13 +20,13 @@ export class VrRankingCategorySelectorComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.allCategories= [
+    this.allCategories = [
       {name: '_Junior_', groups: JR_CATEGORY_HIERARCHY},
       {name: '_Open_', groups: OPEN_RANKING_CATEGORIES},
       {name: '_Adult_', groups: ADULT_CATEGORY_HIERARCHY},
       {name: '_Senior_', groups: SR_CATEGORY_HIERARCHY},
       {name: '_Wheelchair_', groups: WHEELCHAIR_RANKING_CATEGORIES},
-    ]
+    ];
     this.onCategorySelected(OPEN_RANKING_CATEGORIES[0]);
   }
   onCategorySelected(c: RankingCategory) {
